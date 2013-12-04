930689c39c4edb6260dcd9cabedc316af4ae658c
Hubert Wong <hubertwwong@gmail.com>
2013-12-04 00:10:54 -0800

    working on the constructor.

--- lib/mysql2_helper.rb
+++ lib/mysql2_helper.rb
@@ -2,8 +2,28 @@ require 'mysql2'
 
 class Mysql2Helper
   
-  def initalize
+  attr_accessor :client, :url, :user, :password, :db_name
+  
+  def initialize(params = {})
+    @url = params.fetch(:url)
+    @user = params.fetch(:user)
+    @password = params.fetch(:password)
+    @db_name= params.fetch(:db_name)
     
+    @client = self.connect
+  end
+  
+  def import_csv
+    
+  end
+  
+  
+  def connect
+    # connect to db.      
+    return Mysql2::Client.new(:host => @url, 
+                             :database => @db_name,
+                             :username => @user, 
+                             :password => @password)
   end
   
   # TEMP
